6bb39687e42eee39983786dd9e3b4688c9c1a999
alvinahada-afk <alvinahada@gmail.com>
2026-07-25 16:57:06 +0700

    Implement booking function for WhatsApp
    Add booking function to send WhatsApp messages

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 console.log("Website The Animals berhasil dijalankan");
+function bookingWA(){
+
+let nama = document.getElementById("nama").value;
+let hewan = document.getElementById("hewan").value;
+let jenis = document.getElementById("jenis").value;
+let layanan = document.getElementById("layanan").value;
+let tanggal = document.getElementById("tanggal").value;
+
+
+let nomor = "6281234567890"; 
+// Ganti dengan nomor WhatsApp Luxe Pet Grooming
+
+
+let pesan = 
+`Halo Luxe Pet Grooming 🐾
+
+Saya ingin booking grooming.
+
+Nama Pemilik:
+${nama}
+
+Nama Hewan:
+${hewan}
+
+Jenis:
+${jenis}
+
+Layanan:
+${layanan}
+
+Tanggal:
+${tanggal}
+
+Mohon info jadwal tersedia. Terima kasih.`;
+
+
+window.open(
+"https://wa.me/"+nomor+"?text="+encodeURIComponent(pesan),
+"_blank"
+);
+
+}
